118d72b29a6f8ded14d5ac5c0d0ae810a619a2fe
uvantino <yuvrajsingh.c@antino.com>
2024-02-26 16:18:30 +0530

    prettier added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,22 @@ import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => (
-  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+  <Stack
+    direction="row"
+    justifyContent="space-around"
+    sx={{
+      gap: { sm: '123px', xs: '40px' },
+      mt: { sm: '32px', xs: '20px' },
+      justifyContent: 'none',
+    }}
+    px="20px"
+  >
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      <img
+        src={Logo}
+        alt="logo"
+        style={{ width: '48px', height: '48px', margin: '0px 20px' }}
+      />
     </Link>
     <Stack
       direction="row"
@@ -16,14 +29,30 @@ const Navbar = () => (
       fontSize="24px"
       alignItems="flex-end"
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212' }}
-            onMouseOver={(e) => { e.target.style.borderBottom = '3px solid #FF2625'; }}
-            onMouseOut={(e) => { e.target.style.borderBottom = '3px solid transparent'; }}
-      >Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}
-            onMouseOver={(e) => { e.target.style.borderBottom = '3px solid #FF2625'; }}
-            onMouseOut={(e) => { e.target.style.borderBottom = '3px solid transparent'; }}
-      >Exercises</a>
+      <Link
+        to="/"
+        style={{ textDecoration: 'none', color: '#3A1212' }}
+        onMouseOver={(e) => {
+          e.target.style.borderBottom = '3px solid #FF2625';
+        }}
+        onMouseOut={(e) => {
+          e.target.style.borderBottom = '3px solid transparent';
+        }}
+      >
+        Home
+      </Link>
+      <a
+        href="#exercises"
+        style={{ textDecoration: 'none', color: '#3A1212' }}
+        onFocus={(e) => {
+          e.target.style.borderBottom = '3px solid #FF2625';
+        }}
+        onBlur={(e) => {
+          e.target.style.borderBottom = '3px solid transparent';
+        }}
+      >
+        Exercises
+      </a>
     </Stack>
   </Stack>
 );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,25 +1,24 @@
 export const exerciseOptions = {
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
-  params: {limit: '10'},
+  params: { limit: '10' },
   headers: {
     'X-RapidAPI-Key': '959a068b87msh3b0b98c9deb8625p1f5762jsn6427fce4d11b',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
 };
 
-export const youtubeOptions =  {
+export const youtubeOptions = {
   method: 'GET',
   url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
   params: {
-    id: 'UCE_M8A5yxnLfW0KghEeajjw'
+    id: 'UCE_M8A5yxnLfW0KghEeajjw',
   },
   headers: {
     'X-RapidAPI-Key': '959a068b87msh3b0b98c9deb8625p1f5762jsn6427fce4d11b',
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
 };
-
 
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
